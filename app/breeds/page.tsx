e296d670d03ab99406d03a09b8dea90f46e158da
Michael Frangione <mfrangione@gmail.com>
2025-11-06 10:44:21 -0500

    added selects for filters

--- app/breeds/page.tsx
+++ app/breeds/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import { useEffect, useState, useMemo } from "react";
-import { Heading, Grid, TextField, Flex, Link, Container, Separator } from "@radix-ui/themes";
+import { Heading, Grid, TextField, Flex, Link, Container, Separator, Select, Checkbox } from "@radix-ui/themes";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import styles from "./page.module.css";
 import useDebounce from "./hooks/use-debounce";
 
+
 export default function Breeds() {
     const [breeds, setBreeds] = useState<string[]>([]);
     const [search, setSearch] = useState<string>("");
+    const [sortBy, setSortBy] = useState<string>("alphabetical");
+    const [orderBy, setOrderBy] = useState<string>("desc");
+
     const debouncedSearch = useDebounce(search, 300);
 
-    const filteredBreeds = useMemo(() => {
+    const filterAlphabetical = () => {
         // Create a map to store the breeds by the first letter of the breed name and the breeds that start with that letter
         const alphabeticalMap = new Map<string, string[]>();
 
@@ -36,7 +40,17 @@ export default function Breeds() {
 
         // Return the map as an array of entries (letter, breeds)
         return Array.from(alphabeticalMap.entries());
+    };
+
+
+    const filteredBreeds = useMemo(() => {
+        if (sortBy === "alphabetical") {
+            return filterAlphabetical();
+        }
+
+        return breeds;
     }, [breeds, debouncedSearch]);
+
 
     useEffect(() => {
         const fetchBreeds = async () => {
@@ -46,6 +60,7 @@ export default function Breeds() {
                     throw new Error('Failed to fetch breeds');
                 }
                 const data = await response.json();
+                console.log(data);
                 setBreeds(data.breeds || []);
             } catch (error) {
                 console.error('Error fetching breeds:', error);
@@ -67,21 +82,55 @@ export default function Breeds() {
                             <MagnifyingGlassIcon height="16" width="16" />
                         </TextField.Slot>
                     </TextField.Root>
+                    <SearchQuerySelectors sortBy={sortBy} setSortBy={setSortBy} orderBy={orderBy} setOrderBy={setOrderBy} />
                 </div>
-                {filteredBreeds.map(([letter, breeds]) => (
-                    <Flex direction="column" gap="2" my="4" key={letter}>
-                        <Heading size="5" mb="2" color="gray">{letter}</Heading>
-                        <Separator color="gray" style={{ width: '100px', margin: '0 auto' }} />
-                        <Grid columns={{ initial: '1', sm: '2', md: '3', lg: '4' }} gap="5" mt="6" align="start" className={styles.breedsGrid}>
-                            {breeds.map((breed) => (
-                                <Link href={`/breeds/${breed}`} key={breed} size="3" mb="2">
-                                    {breed}
-                                </Link>
-                            ))}
-                        </Grid>
-                    </Flex>
-                ))}
+                {sortBy === "alphabetical" ?
+                    filteredBreeds.map(([letter, breeds]) => (
+                        <Flex direction="column" gap="2" my="4" key={letter}>
+                            <Heading size="5" mb="2" color="gray">{letter}</Heading>
+                            <Separator color="gray" style={{ width: '100px', margin: '0 auto' }} />
+                            <Grid columns={{ initial: '1', sm: '2', md: '3', lg: '4' }} gap="5" mt="6" align="start" className={styles.breedsGrid}>
+                                {breeds.map((breed: string) => (
+                                    <Link href={`/breeds/${breed}`} key={breed} size="3" mb="2">
+                                        {breed}
+                                    </Link>
+                                ))}
+                            </Grid>
+                        </Flex>
+                    )) :
+                    <Grid columns={{ initial: '1', sm: '2', md: '3', lg: '4' }} gap="5" mt="6" align="start" className={styles.breedsGrid}>
+                        {breeds.map((breed) => (
+                            <Link href={`/breeds/${breed}`} key={breed} size="3" mb="2">
+                                {breed}
+                            </Link>
+                        ))}
+                    </Grid>}
             </Flex>
         </Container>
     );
 }
+
+const SearchQuerySelectors = ({ sortBy, setSortBy, orderBy, setOrderBy }: { sortBy: string, setSortBy: (value: string) => void, orderBy: string, setOrderBy: (value: string) => void; }) => {
+
+
+    return (
+        <Flex direction="row" gap="2">
+            <Select.Root defaultValue={sortBy} value={sortBy} onValueChange={setSortBy}>
+                <Select.Trigger placeholder="Sort By" />
+                <Select.Content>
+                    <Select.Item value="alphabetical">Alphabetical</Select.Item>
+                    <Select.Item value="popularity">Popularity</Select.Item>
+                    <Select.Item value="weight">Weight</Select.Item>
+                    <Select.Item value="height">Height</Select.Item>
+                </Select.Content>
+            </Select.Root>
+            <Select.Root defaultValue={orderBy} value={orderBy} onValueChange={setOrderBy}>
+                <Select.Trigger placeholder="Order By" />
+                <Select.Content>
+                    <Select.Item value="asc">Ascending</Select.Item>
+                    <Select.Item value="desc">Descending</Select.Item>
+                </Select.Content>
+            </Select.Root>
+        </Flex >
+    );
+};
